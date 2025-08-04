c373d5c1219ec6dd8d710fa4d61eea852e37259c
DESKTOP-85SG08H\AB <batyhtik3@mail.ru>
2025-08-04 17:32:14 +0300

    refactor: change name for State

---
+++ backend/WarehouseManagement/WarehouseManagement.Domain/Enums/EState.cs
@@ -0,0 +1,16 @@
+﻿namespace WarehouseManagement.Domain.Enums;
+
+/// <summary>
+/// Состояние
+/// </summary>
+public enum EState
+{
+    /// <summary>
+    /// Активное
+    /// </summary>
+    Active = 1,
+    /// <summary>
+    /// Архивированное
+    /// </summary>
+    Archived = 2
+}
