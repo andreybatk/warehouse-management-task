d6fae4e25b42f31de2cb152f97272c3eed991042
DESKTOP-85SG08H\AB <batyhtik3@mail.ru>
2025-08-06 20:38:31 +0300

    feat: created get numbers documents

---
+++ backend/WarehouseManagement/WarehouseManagement.BusinessLogic/ReceiptDocuments/Handlers/GetNumbersDocumentsQueryHandler.cs
@@ -0,0 +1,13 @@
+﻿using WarehouseManagement.BusinessLogic.Abstractions.Messaging;
+using WarehouseManagement.BusinessLogic.ReceiptDocuments.Queries;
+using WarehouseManagement.Domain.Interfaces;
+
+namespace WarehouseManagement.BusinessLogic.ReceiptDocuments.Handlers;
+public class GetNumbersDocumentsQueryHandler(IReceiptDocumentRepository receiptDocumentRepository)
+    : IQueryHandler<GetNumbersDocumentsQuery, List<long>>
+{
+    public async Task<List<long>> Handle(GetNumbersDocumentsQuery request, CancellationToken cancellationToken)
+    {
+        return await receiptDocumentRepository.GetNumbersAsync();
+    }
+}
---
+++ backend/WarehouseManagement/WarehouseManagement.BusinessLogic/ReceiptDocuments/Queries/GetNumbersDocumentsQuery.cs
@@ -0,0 +1,8 @@
+﻿using WarehouseManagement.BusinessLogic.Abstractions.Messaging;
+using WarehouseManagement.BusinessLogic.ReceiptDocuments.DTOs;
+
+namespace WarehouseManagement.BusinessLogic.ReceiptDocuments.Queries;
+/// <summary>
+/// Запрос на получение номеров документа поступления
+/// </summary>
+public sealed record GetNumbersDocumentsQuery : IQuery<List<long>>;
